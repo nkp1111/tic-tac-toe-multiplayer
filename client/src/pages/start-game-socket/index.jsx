359de871ebfa366b7b4c2b136576ca70f3730317
nkp1111 <neerajp4321@gmail.com>
2023-10-24 13:33:04 +0530

    feat:💄👔 add game board and modal with room id

--- client/src/pages/start-game-socket/index.jsx
+++ client/src/pages/start-game-socket/index.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { useSearchParams } from "react-router-dom";
 import { io } from "socket.io-client";
+import GameBoard from '../../components/gameBoard';
+import PlayerWaitModal from './playerWaitModal';
 
 let loadingToast;
 
@@ -12,6 +14,11 @@ export default function StartGameSocket() {
     : `Player-#${Math.floor(Math.random() * 100000)}`;
   const [socket, setSocket] = useState(null);
   const [gameRoomId, setGameRoomId] = useState("");
+  const [gameStats, setGameStats] = useState({
+    startGame: false,
+    playerTurn: false,
+    opponentName: "",
+  });
 
   useEffect(() => {
     const serverUrl = import.meta.env.VITE_SERVER_URL || "http://localhost:3000";
@@ -46,18 +53,37 @@ export default function StartGameSocket() {
       socket.on(`${gameRoomId} message`, (response) => {
         console.log(response);
         console.log("game room message")
+        // game leaving message
         if (response.id === 3) {
           console.log(response.success);
         }
       })
-
     })
   }, [gameRoomId, socket])
 
 
   return (
-    <div>
-      Start Game With Socket  {playerName}
+    <div className='p-4'>
+      <div className={`${!gameStats.startGame && "bg-gray-500 opacity-50 w-full h-full fixed top-0 left-0 z-10"}`} />
+
+      {/* wait until player arrive modal */}
+      <PlayerWaitModal gameRoomId={gameRoomId} gameStats={gameStats} />
+
+      <h1 className='font-bold text-6xl sm:text-4xl text-center'>Tic Tac Toe</h1>
+      <GameBoard />
+
+      {/* show both players */}
+      <div className='flex flex-row sm:flex-col my-10 justify-center align-middle'>
+        <div className='border-r-2 border-white px-10 flex gap-3'>
+          <h2 className='text-lg font-semibold'>{playerName}</h2>
+          <span className={`badge badge-info ${gameStats.startGame && gameStats.playerTurn ? "block" : "hidden"}`} />
+        </div>
+
+        <div className='border-l-2 border-white px-10 flex gap-3'>
+          <h2 className='text-lg font-semibold'>{gameStats.opponentName}</h2>
+          <span className={`badge badge-info ${gameStats.startGame && !gameStats.playerTurn ? "block" : "hidden"}`} />
+        </div>
+      </div>
     </div>
   )
 }
---
+++ client/src/pages/start-game-socket/playerWaitModal.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import toast from 'react-hot-toast';
+
+const modalOpenStyles = "block bg-neutral absolute"
+const modalCloseStyles = "hidden"
+
+export default function PlayerWaitModal(
+  // eslint-disable-next-line react/prop-types
+  { gameRoomId, gameStats: { startGame } }
+) {
+  return (
+    <div className={`text-white bg-neutral card p-4 absolute md:w-1/2 md:left-1/3 top-5 z-20 w-auto left-5 right-5 ${!startGame ? modalOpenStyles : modalCloseStyles}`}>
+      <div className="flex flex-col mt-1 justify-center align-middle text-center">
+        <h3 className='font-bold text-2xl'>Waiting for Player to connect <span className='animate-ping'>...</span></h3>
+        <p>Give your friend the following room Id to connect</p>
+        <hr className='w-1/2 my-5 mx-auto' />
+        <p className='text-2xl'>{gameRoomId}</p>
+        <button className='btn btn-info mx-auto mt-4 px-5'
+          onClick={() => {
+            navigator.clipboard.writeText(gameRoomId);
+            toast.success(`Copied game room id \n${gameRoomId}`);
+          }}>Copy</button>
+      </div>
+    </div>
+  )
+}
+
